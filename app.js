ed7a154cf1d2aff3647179a1eb2dc15a48e9266e
Jayasrithiyagarajan <104126539+Jayasrithiyagarajan@users.noreply.github.com>
2025-01-21 14:43:05 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
+const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -57,4 +57,3 @@ app.use((err, req, res, next) => {
 // });
  
 module.exports = app;
- 
